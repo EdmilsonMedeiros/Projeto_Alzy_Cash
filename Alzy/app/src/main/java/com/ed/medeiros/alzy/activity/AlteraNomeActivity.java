34bb5c6773eb76efdcd94c552f119842ff5f2239
EdmilsonMedeiros <edmilson.mprojetos@gmail.com>
2020-10-10 13:25:50 -0300

    Refatorando. Exibição de e-mail na tela alterção de nome.

--- Alzy/app/src/main/java/com/ed/medeiros/alzy/activity/AlteraNomeActivity.java
+++ Alzy/app/src/main/java/com/ed/medeiros/alzy/activity/AlteraNomeActivity.java
@@ -5,17 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.ed.medeiros.alzy.R;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class AlteraNomeActivity extends AppCompatActivity {
+    private TextView textViewEmail;
     private FirebaseAuth autenticacao = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_altera_nome);
+        textViewEmail = findViewById(R.id.textViewEmail);
     }
     public void sair(View view){
         finish();
@@ -24,6 +27,7 @@ public class AlteraNomeActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        textViewEmail.setText(autenticacao.getCurrentUser().getEmail());
         if (autenticacao.getCurrentUser() == null){
             startActivity(new Intent(this, SplashActivity.class));
         }
