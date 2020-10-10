7e890e31f6374b556195d1b2c5ad18bb86c9b3f6
EdmilsonMedeiros <edmilson.mprojetos@gmail.com>
2020-10-10 13:30:01 -0300

    Refatorando. Exibição de e-mail na tela alterção de e-mail.

--- Alzy/app/src/main/java/com/ed/medeiros/alzy/activity/AlteraEmailActivity.java
+++ Alzy/app/src/main/java/com/ed/medeiros/alzy/activity/AlteraEmailActivity.java
@@ -5,17 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.ed.medeiros.alzy.R;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class AlteraEmailActivity extends AppCompatActivity {
     private FirebaseAuth autenticacao = FirebaseAuth.getInstance();
+    private TextView textViewEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_altera_email);
+        textViewEmail = findViewById(R.id.textViewEmail);
     }
     public void sair(View view){
         finish();
@@ -24,6 +27,7 @@ public class AlteraEmailActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        textViewEmail.setText(autenticacao.getCurrentUser().getEmail());
         if (autenticacao.getCurrentUser() == null){
             startActivity(new Intent(this, SplashActivity.class));
         }
